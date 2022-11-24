bba2f73d64d719ab203bc70bbf0a826a5486b259
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-11-24 16:48:38 +0100

    Idea

--- webapp/src/main/scala/webapp/services/WebRTCService.scala
+++ webapp/src/main/scala/webapp/services/WebRTCService.scala
@@ -28,7 +28,54 @@ import scala.reflect.Selectable.*
 import scala.scalajs.js
 import webapp.*
 import webapp.pages.*
+import kofre.decompose.containers.DeltaBufferRDT
+import kofre.decompose.interfaces.LWWRegisterInterface.LWWRegister
+import java.util.concurrent.ThreadLocalRandom
+import kofre.decompose.interfaces.LWWRegisterInterface
+import kofre.decompose.interfaces.MVRegisterInterface.MVRegisterSyntax
+import kofre.datatypes.TimedVal
+
+import kofre.datatypes.TimedVal
+import kofre.decompose.containers.DeltaBufferRDT
+import kofre.decompose.interfaces.LWWRegisterInterface
+import kofre.decompose.interfaces.LWWRegisterInterface.LWWRegisterSyntax
+import kofre.decompose.interfaces.LWWRegisterInterface.LWWRegister
+import kofre.decompose.interfaces.MVRegisterInterface.MVRegisterSyntax
+import kofre.dotted.Dotted
+import kofre.syntax.DottedName
+import loci.registry.Binding
+import org.scalajs.dom.UIEvent
+import org.scalajs.dom.html.{Input, LI}
+import rescala.default.*
+import rescala.extra.Tags.*
+import scala.Function.const
+import scala.collection.mutable
+import scala.scalajs.js.timers.setTimeout
 
 class WebRTCService() {
   val registry = new Registry
+
+  val replicaId: String = ThreadLocalRandom.current().nextLong().toHexString
+
+  def test() = {
+    val lwwInit = DeltaBufferRDT(replicaId, LWWRegisterInterface.empty[Int])
+
+    val lww: DeltaBufferRDT[LWWRegister[Int]] = MVRegisterSyntax(lwwInit).write(TimedVal(0, lwwInit.replicaID, 0, 0));
+
+    val testValue = rescala.default.Event[Int](None);
+
+    val deltaEvt = Evt[DottedName[LWWRegister[Int]]]()
+
+    // TODO FIXME Storing.storedAs persists this value
+
+    // createTaskRef
+    val counterSignalPeerA: Signal[DeltaBufferRDT[LWWRegister[Int]]] = Events.foldAll(lww)(current => {
+      Seq(
+        testValue.act2({ v => current.resetDeltaBuffer().map(_ => v) }),
+        deltaEvt.act2({ delta => current.resetDeltaBuffer().applyDelta(delta) }),
+      )
+    })
+
+    // testValue.set(1)
+  }
 }
