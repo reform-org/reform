84792c8a5c1e13148c18ac0996882fdcebf87c12
Oshgnacknak <osh@oshgnacknak.de>
2022-11-28 22:16:41 +0100

    WIP: allow adding and removing projects

--- webapp/src/main/scala/webapp/pages/ProjectPage.scala
+++ webapp/src/main/scala/webapp/pages/ProjectPage.scala
@@ -16,46 +16,76 @@ limitations under the License.
 package webapp.pages
 
 import org.scalajs.dom
+import org.scalajs.dom.window
 import outwatch.*
 import outwatch.dsl.*
 import rescala.default.*
 import webapp.services.*
 import webapp.*
+import webapp.given
 import webapp.components.navigationHeader
 
+case class Project(name: String, maxHours: Int, account: Option[String])
+
 case class ProjectPage(id: String) extends Page {
+
+  private val projects: Var[List[Project]] = Var(
+    List(
+      Project("FOP", Integer.MAX_VALUE, Some("KW")),
+      Project("Reform", 69, None),
+      Project("Off-Topic", 0, Some("HK")),
+    ),
+  )
+
+  private val name = Var("")
+
   def render(using services: Services): VNode =
     div(
       navigationHeader,
+      input(
+        name,
+        onInput.value --> name,
+      ),
+      button(
+        cls := "btn",
+        "New",
+        onClick.foreach(_ => addProject(name.now)),
+      ),
       table(
         cls := "table-auto",
         thead(
           tr(
-            th("Song"),
-            th("Artist"),
-            th("Year"),
+            th("Project"),
+            th("Max Hours"),
+            th("Account"),
           ),
         ),
-        tbody(
-          tr(
-            td(
-              cls := "text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap  nowrap",
-              "The Sliding Mr. Bones (Next Stop, Pottersville)",
-            ),
-            td("Malcolm Lockyer"),
-            td("1961"),
-          ),
-          tr(
-            td("Witchy Woman"),
-            td("The Eagles"),
-            td("1972"),
-          ),
-          tr(
-            td("Shining Star"),
-            td("Earth, Wind, and Fire"),
-            td("1975"),
-          ),
-        ),
+        projects.map(renderProjects),
       ),
     )
+
+  private def renderProjects(projects: List[Project]): VNode =
+    tbody(projects.map { p =>
+      tr(
+        td(p.name),
+        td(p.maxHours),
+        td(p.account.getOrElse("-")),
+        button(
+          cls := "btn",
+          "Delete",
+          onClick.foreach(_ => removeProject(p)),
+        ),
+      )
+    })
+
+  def removeProject(p: Project): Unit = {
+    val yes = window.confirm(s"Do you really want to delete the project \"${p.name}\"?")
+    if (yes) {
+      projects.transform(_.filterNot(_ == p))
+    }
+  }
+
+  def addProject(name: String): Unit = {
+    projects.transform(_.appended(Project(name, 0, None)))
+  }
 }
