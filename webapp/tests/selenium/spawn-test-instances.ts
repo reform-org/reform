71d8a49dab237a7f0c838d1fff3b26235b80af1a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-01-24 21:11:04 +0100

    Create a script to autostart test instances

---
+++ webapp/tests/selenium/spawn-test-instances.ts
@@ -0,0 +1,70 @@
+import { parseArgs } from "node:util";
+import { Peer } from "./lib.js";
+import readline from "readline";
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+console.log(
+	"usage: npm run spawn-test-instances -- [--count count] [--url ...]",
+);
+
+const m = process.version.match(/(\d+)\.(\d+)\.(\d+)/)!;
+const [major, minor, patch] = m.slice(1).map((_) => parseInt(_));
+
+if (!(major > 18 || (major == 18 && minor >= 11))) {
+	console.warn(`\x1b[41m\x1b[30m
+┌──────────────────────────────────────────────────────────────────┐
+│                                                                  │
+│    NodeJS is less than v18.11.0, this version is unsupported!    │
+│                                                                  │
+└──────────────────────────────────────────────────────────────────┘\x1b[0m`);
+}
+
+let {
+	values: { count, url },
+} = parseArgs({
+	options: {
+		count: {
+			type: "string",
+			default: "2",
+			short: "c",
+		},
+		url: {
+			type: "string",
+			default: "http://localhost:5173/",
+		},
+	},
+});
+
+console.log(`Starting ${count} instances on ${url}`);
+
+let peers = await Promise.all(
+	[...Array(Number(count))].map(async () => {
+		let peer = await Peer.create(false);
+		return peer;
+	}),
+);
+
+try {
+	const it = rl[Symbol.asyncIterator]();
+	while (true) {
+		console.log("CONNECTING ALL");
+		await Promise.all(peers.map((peer) => peer.driver.get(url!)));
+
+		await peers
+			.map((v) => Promise.resolve(v))
+			.reduce(async (prev, curr) => {
+				await (await prev).connectTo(await curr);
+				await (await curr).driver.sleep(100);
+				return await curr;
+			});
+		console.log("ALL CONNECTED");
+
+		const line = await it.next();
+	}
+} finally {
+	await Promise.allSettled(peers.map((peer) => peer.driver.quit()));
+}
