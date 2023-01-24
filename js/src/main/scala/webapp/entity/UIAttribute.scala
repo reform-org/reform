2cdd738be10445a1a1aaada3cc2700d31949f2fa
Oshgnacknak <osh@oshgnacknak.de>
2023-01-24 17:40:51 +0100

    Basic UIAttributeBuilder

--- js/src/main/scala/webapp/entity/UIAttribute.scala
+++ js/src/main/scala/webapp/entity/UIAttribute.scala
@@ -46,19 +46,3 @@ case class UIAttribute[EntityType, AttributeType](
     }
   }
 }
-
-object UIAttribute {
-
-  /* def string[EntityType](setter: (EntityType, String) => EntityType): AttributeHandler[EntityType, String]
-  = AttributeHandler(identity, identity, setter)
-
-  def int[EntityType](setter: (EntityType, Int) => EntityType): AttributeHandler[EntityType, Int]
-  = AttributeHandler(_.toString, _.toInt, setter)
-
-  def optionWithDefault[EntityType, AttributeType](default: AttributeType, attr: AttributeHandler[EntityType, AttributeType]): AttributeHandler[EntityType, Option[AttributeType]]
-  = AttributeHandler(
-    v => attr.readConverter(v.getOrElse(default)),
-    s => Some(attr.writeConverter(s)),
-    (e, v) => attr.setter(e, v.getOrElse(default))
-  ) */
-}
