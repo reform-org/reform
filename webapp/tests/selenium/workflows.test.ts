ad222aff5d4b698c16a01d00222d52e3f9832c00
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-01-27 22:31:57 +0100

    Add examples for workflow tests (#141)

---
+++ webapp/tests/selenium/workflows.test.ts
@@ -0,0 +1,50 @@
+import { Peer } from "./lib.js";
+import { assert, describe, expect, it, test } from "vitest";
+
+let headless = true;
+
+async function startPeers(count: number) {
+	return await Promise.all(
+		[...Array(Number(count))].map(async () => {
+			let peer = await Peer.create(headless);
+			return peer;
+		}),
+	);
+}
+
+async function loadPage(peers: Peer[]) {
+	await Promise.all(
+		peers.map(async (peer) => peer.driver.get("http://localhost:5173/")),
+	);
+}
+
+async function quitPeers(peers: Peer[]) {
+	await Promise.allSettled(peers.map((peer) => peer.driver.quit()));
+}
+
+// tests that can also run on mac:
+describe.concurrent("safari-compatible", () => {
+	it("creates project", async () => {
+		let [peer] = await startPeers(1);
+		await loadPage([peer]);
+		await peer.createProject();
+		await quitPeers([peer]);
+	});
+
+	it("loads page", async () => {
+		let [peer] = await startPeers(1);
+		await loadPage([peer]);
+		await quitPeers([peer]);
+	});
+});
+
+describe
+	.skipIf(process.env.SELENIUM_BROWSER === "safari")
+	.concurrent("safari-incompatible", () => {
+		it("connects", async () => {
+			let peers = await startPeers(2);
+			await loadPage(peers);
+			await peers[0].connectTo(peers[1]);
+			await quitPeers(peers);
+		});
+	});
