6684e4423111389da47bd16503f6ac3eb1ee6c99
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-02-10 14:44:26 +0100

    Use random seed again (#207)

--- webapp/tests/selenium/lib.ts
+++ webapp/tests/selenium/lib.ts
@@ -6,8 +6,8 @@ import safari from "selenium-webdriver/safari.js";
 import { Chance } from "chance";
 import { strict as assert } from "node:assert";
 
-//export let seed = new Chance().integer();
-export let seed = 7196640142163968;
+export let seed = new Chance().integer();
+//export let seed = 7196640142163968;
 export var chance = new Chance(seed);
 console.log(`The seed is: ${chance.seed}`);
 
