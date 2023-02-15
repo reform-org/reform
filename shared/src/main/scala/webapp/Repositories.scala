a03cb4bdc295584c7be8c66c857898fe81c8103d
lukasschreiber <info@lukasschreiber.com>
2023-02-15 15:54:33 +0100

    rename repos to match main

--- shared/src/main/scala/webapp/Repositories.scala
+++ shared/src/main/scala/webapp/Repositories.scala
@@ -11,9 +11,9 @@ class Repositories(using registry: Registry, indexedDb: IIndexedDB) {
   val users: Repository[User] = Repository("user", User.empty)
   val hiwis: Repository[Hiwi] = Repository("hiwi", Hiwi.empty)
   val supervisors: Repository[Supervisor] = Repository("supervisor", Supervisor.empty)
-  val contractSchemas: Repository[ContractSchema] = Repository("contractSchema", ContractSchema.empty)
-  val paymentLevels: Repository[PaymentLevel] = Repository("paymentLevels", PaymentLevel.empty)
-  val salaryChanges: Repository[SalaryChange] = Repository("salaryChanges", SalaryChange.empty)
+  val contractSchemas: Repository[ContractSchema] = Repository("contract-schema", ContractSchema.empty)
+  val paymentLevels: Repository[PaymentLevel] = Repository("payment-level", PaymentLevel.empty)
+  val salaryChanges: Repository[SalaryChange] = Repository("salary-change", SalaryChange.empty)
   val requiredDocuments: Repository[RequiredDocument] = Repository("required-document", RequiredDocument.empty)
   val contracts: Repository[Contract] = Repository("contracts", Contract.empty)
 }
