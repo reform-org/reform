0f01299293bf33dfc8d67e44702dd1eb1baa1b2e
lukasschreiber <info@lukasschreiber.com>
2023-02-16 22:29:26 +0100

    add 404 error page

---
+++ js/src/main/scala/webapp/pages/ErrorPage.scala
@@ -0,0 +1,37 @@
+package webapp.pages
+
+import webapp.services.*
+import webapp.webrtc.WebRTCService
+import outwatch.*
+import outwatch.dsl.*
+import webapp.components.navigationHeader
+import webapp.*
+import org.scalajs.dom.HTMLElement
+
+case class ErrorPage() extends Page {
+
+  def render(using
+      routing: RoutingService,
+      repositories: Repositories,
+      webrtc: WebRTCService,
+      discovery: DiscoveryService,
+      toaster: Toaster,
+  ): VNode = {
+    navigationHeader(
+      div(
+        cls := "flex items-center justify-center h-full w-full flex-col gap-6",
+        h1("404 | Page not found", cls := "text-6xl text-slate-200"),
+        a(
+          "Take me Home",
+          cls := "text-blue-600 text-lg",
+          onClick.foreach(e => {
+            e.preventDefault()
+            e.target.asInstanceOf[HTMLElement].blur()
+            routing.to(HomePage(), true)
+          }),
+          href := routing.linkPath(HomePage()),
+        ),
+      ),
+    )
+  }
+}
