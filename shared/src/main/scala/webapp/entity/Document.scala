618a9be1fca78458c4cfeeb991ce9adf437fc0d2
Oshgnacknak <osh@oshgnacknak.de>
2023-02-28 17:21:23 +0100

    Remove file name from document

--- shared/src/main/scala/webapp/entity/Document.scala
+++ shared/src/main/scala/webapp/entity/Document.scala
@@ -8,7 +8,6 @@ import webapp.entity.Attribute.given
 
 case class Document(
     name: Attribute[String] = Attribute.empty,
-    fileName: Attribute[String] = Attribute.empty,
     _exists: Attribute[Boolean] = Attribute.empty,
 ) extends Entity[Document]
     derives DecomposeLattice,
@@ -16,7 +15,7 @@ case class Document(
 
   // empty for required fields, default for optional fields
   def default: Document =
-    Document(Attribute.default, Attribute.default, Attribute(true))
+    Document(Attribute.default, Attribute(true))
 
   def identifier: Attribute[String] = name
 
