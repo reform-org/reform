bacb9c654c06dca664d66e9732a6a87b7b7072a9
lukasschreiber <info@lukasschreiber.com>
2023-03-11 21:26:29 +0100

    add checkbox labels (#465)
    * add checkbox labels
    
    * calculate hours below limit
    
    * some nice calculations
    
    * even cooler calculations
    
    * fire ctrl s event only once after page reload by explicitly converting scala fun to js fun
    
    * remove console log
    
    * make exceptions infinit but dismissable

--- js/src/main/scala/webapp/entity/UIAttribute.scala
+++ js/src/main/scala/webapp/entity/UIAttribute.scala
@@ -149,6 +149,7 @@ class UITextAttribute[EntityType, AttributeType](
         val (editStart, entityVar) = editing
         val editStartAttr = getter(editStart)
         div(
+          cls := "relative min-w-[1rem]",
           Signal {
             renderEditInput(
               formId,
@@ -296,6 +297,7 @@ class UICheckboxAttribute[EntityType](
       entity: EntityType,
   ): VMod = Checkbox(
     CheckboxStyle.Default,
+    cls := "absolute top-1/2 -translate-y-1/2",
     formId := _formId,
     checked <-- attr.map(_.get.getOrElse(false)),
     onClick.foreach(_ => set(!attr.now.get.getOrElse(false))),
