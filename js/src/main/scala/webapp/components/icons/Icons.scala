2d99abb163d14041c45e2f5934a866e07cab3893
JuliaSelua <104761063+JuliaSelua@users.noreply.github.com>
2023-03-14 22:14:21 +0100

    design fixes

--- js/src/main/scala/webapp/components/icons/Icons.scala
+++ js/src/main/scala/webapp/components/icons/Icons.scala
@@ -556,3 +556,29 @@ object Settings extends Icon {
     )
   }
 }
+
+object Return extends Icon {
+  def apply(props: VMod*): VNode = {
+    import svg.*
+    svg(
+      xmlns := "http://www.w3.org/2000/svg",
+      viewBox := "0 0 48 48",
+      props,
+      fill := "none",
+      path(
+        d := "M12.9998 8L6 14L12.9998 21",
+        stroke := "currentColor",
+        VMod.attr("stroke-width") := "4",
+        VMod.attr("stroke-linecap") := "round",
+        VMod.attr("stroke-linejoin") := "round",
+      ),
+      path(
+        d := "M6 14H28.9938C35.8768 14 41.7221 19.6204 41.9904 26.5C42.2739 33.7696 36.2671 40 28.9938 40H11.9984",
+        stroke := "currentColor",
+        VMod.attr("stroke-width") := "4",
+        VMod.attr("stroke-linecap") := "round",
+        VMod.attr("stroke-linejoin") := "round",
+      ),
+    )
+  }
+}
