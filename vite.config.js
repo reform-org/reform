328f7c94ddf5f51effd6a2e49d2ce5d1fc3bbb1f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-03-15 16:19:06 +0100

    Service worker (#321)
    * tmp
    
    * Config
    
    * pwa config
    
    * fix config
    
    * Fix
    
    * Add package lock
    
    * Update icons
    
    * Update
    
    * Some fixess
    
    * jo
    
    * Fix
    
    * Fix
    
    ---------
    
    Co-authored-by: lukasschreiber <info@lukasschreiber.com>

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,59 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { visualizer } from "rollup-plugin-visualizer";
 
 /** @type {import('vitest/config').UserConfig} */
 export default {
 	plugins: [
-		//basicSsl(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'inline',
+			workbox: {
+				maximumFileSizeToCacheInBytes: 10 * 1000 * 1000,
+				navigateFallbackDenylist: [
+					// TODO FIXME configure based on env variables
+					/^\/discovery-server-websocket/,
+					/^\/always-online-peer/,
+					/^\/api/,
+					/.*\.js\.map/,
+				],
+				clientsClaim: true,
+				skipWaiting: true,
+			},
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'safari-pinned-tab.svg', 'favicon-32x32.png', 'favicon-16x16.png'],
+			manifest: {
+				"name": "ReForm",
+				"short_name": "ReForm",
+				"icons": [
+					{
+						"src": "/android-chrome-192x192.png",
+						"sizes": "192x192",
+						"type": "image/png"
+					},
+					{
+						"src": "/android-chrome-512x512.png",
+						"sizes": "512x512",
+						"type": "image/png"
+					},
+					{
+						src: '/android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable'
+					}
+				],
+				"theme_color": "#ffffff",
+				"background_color": "#ffffff",
+				"display": "standalone"
+			}
+		}),
+		//basicSsl()
 		visualizer()
 	],
+	build: {
+		sourcemap: true,
+	},
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
 	},
@@ -17,9 +64,6 @@ export default {
 	},
 	preview: {
 		port: 5173,
-	},
-	build: {
-		sourcemap: true
 	},
 	clearScreen: false,
 };
