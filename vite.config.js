0f007e1827a02d6695bcfbe8ad9171ecc35a7d77
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-03-15 14:46:14 +0100

    Update icons

--- vite.config.js
+++ vite.config.js
@@ -24,12 +24,6 @@ export default {
 						"src": "/android-chrome-512x512.png",
 						"sizes": "512x512",
 						"type": "image/png"
-					},
-					{
-						src: '/android-chrome-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any maskable'
 					}
 				],
 				"theme_color": "#ffffff",
