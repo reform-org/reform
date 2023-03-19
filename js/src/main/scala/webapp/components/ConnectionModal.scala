3c4daaa4dd8ef381038af462a153401b11959691
Lukas Schreiber <info@lukasschreiber.com>
2023-03-20 00:44:07 +0100

    remove close connection buttons as they so not work as intended

--- js/src/main/scala/webapp/components/ConnectionModal.scala
+++ js/src/main/scala/webapp/components/ConnectionModal.scala
@@ -61,7 +61,7 @@ class ConnectionModal(using jsImplicits: JSImplicits) {
         Signal {
           jsImplicits.discovery.token.value.map(t =>
             span(
-              cls := "inline-flex gap-2cflex-row",
+              cls := "inline-flex gap-2 flex-row",
               i(jsImplicits.discovery.decodeToken(t).username),
               if (jsImplicits.discovery.decodeToken(t).tpe == "SSO") Some(icons.CheckCircle(cls := "w-4 h-4")) else None,
             ),
