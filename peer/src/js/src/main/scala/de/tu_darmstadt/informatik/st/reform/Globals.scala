a9412f72b09a51bafc7c676341ff170dac01b5c2
Oshgnacknak <osh@oshgnacknak.de>
2023-11-21 17:33:08 +0100

    Make env vars lazy

--- peer/src/js/src/main/scala/de/tu_darmstadt/informatik/st/reform/Globals.scala
+++ peer/src/js/src/main/scala/de/tu_darmstadt/informatik/st/reform/Globals.scala
@@ -24,60 +24,74 @@ abstract case class JSImplicits() {
 }
 
 object Env {
+  private val env = js.`import`.meta.env
+
   def get(name: String): String = {
-    val opt = js.`import`.meta.env.selectDynamic(name)
-    if (opt == js.undefined) {
+    val opt: Option[String] = maybeGet(name)
+
+    if (opt.isEmpty) {
       throw new IllegalStateException(s"Environment variable ${name} must be set. (Did you source .env)?")
     }
-    opt.asInstanceOf[String]
+
+    opt.get
   }
 
   def getOrElse(name: String, default: String): String = {
-    val opt = js.`import`.meta.env.selectDynamic(name)
-    if (opt == js.undefined) {
-      return default
+    maybeGet(name).getOrElse(default)
+  }
+
+  private def maybeGet(name: String): Option[String] = {
+    if (env == js.undefined) {
+      return sys.env.get(name)
     }
-    opt.asInstanceOf[String]
+
+    val x = env.selectDynamic(name)
+
+    if (x == js.undefined) {
+      return None
+    }
+
+    Some(x.asInstanceOf[String])
   }
 }
 
 object Globals {
-  val VITE_SELENIUM: Boolean = Env.getOrElse("VITE_SELENIUM", "false") == "true"
+  lazy val VITE_SELENIUM: Boolean = Env.getOrElse("VITE_SELENIUM", "false") == "true"
 
-  val VITE_DATABASE_VERSION: String = Env.get("VITE_DATABASE_VERSION")
+  lazy val VITE_DATABASE_VERSION: String = Env.get("VITE_DATABASE_VERSION")
 
-  val VITE_PROTOCOL_VERSION: String = Env.getOrElse("VITE_PROTOCOL_VERSION", "test")
+  lazy val VITE_PROTOCOL_VERSION: String = Env.getOrElse("VITE_PROTOCOL_VERSION", "test")
 
-  val VITE_SERVER_PROTOCOL: String = Env.get("VITE_SERVER_PROTOCOL")
-  val VITE_SERVER_HOST: String = Env.get("VITE_SERVER_HOST")
-  val VITE_SERVER_PATH: String = Env.get("VITE_SERVER_PATH")
-  val VITE_SERVER_PORT: String = Env.get("VITE_SERVER_PORT")
+  lazy val VITE_SERVER_PROTOCOL: String = Env.get("VITE_SERVER_PROTOCOL")
+  lazy val VITE_SERVER_HOST: String = Env.get("VITE_SERVER_HOST")
+  lazy val VITE_SERVER_PATH: String = Env.get("VITE_SERVER_PATH")
+  lazy val VITE_SERVER_PORT: String = Env.get("VITE_SERVER_PORT")
 
-  val VITE_DISCOVERY_SERVER_PROTOCOL: String = Env.get("VITE_DISCOVERY_SERVER_PROTOCOL")
-  val VITE_DISCOVERY_SERVER_HOST: String = Env.get("VITE_DISCOVERY_SERVER_HOST")
-  val VITE_DISCOVERY_SERVER_PATH: String = Env.get("VITE_DISCOVERY_SERVER_PATH")
+  lazy val VITE_DISCOVERY_SERVER_PROTOCOL: String = Env.get("VITE_DISCOVERY_SERVER_PROTOCOL")
+  lazy val VITE_DISCOVERY_SERVER_HOST: String = Env.get("VITE_DISCOVERY_SERVER_HOST")
+  lazy val VITE_DISCOVERY_SERVER_PATH: String = Env.get("VITE_DISCOVERY_SERVER_PATH")
   // TODO: whats the difference between the two?
-  val VITE_DISCOVERY_SERVER_LISTEN_PORT: String = Env.get("VITE_DISCOVERY_SERVER_LISTEN_PORT")
-  val VITE_DISCOVERY_SERVER_PUBLIC_PORT: String = Env.get("VITE_DISCOVERY_SERVER_PUBLIC_PORT")
+  lazy val VITE_DISCOVERY_SERVER_LISTEN_PORT: String = Env.get("VITE_DISCOVERY_SERVER_LISTEN_PORT")
+  lazy val VITE_DISCOVERY_SERVER_PUBLIC_PORT: String = Env.get("VITE_DISCOVERY_SERVER_PUBLIC_PORT")
 
-  val VITE_DISCOVERY_SERVER_WEBSOCKET_PROTOCOL: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_PROTOCOL")
-  val VITE_DISCOVERY_SERVER_WEBSOCKET_HOST: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_HOST")
-  val VITE_DISCOVERY_SERVER_WEBSOCKET_PATH: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_PATH")
-  val VITE_DISCOVERY_SERVER_WEBSOCKET_SUBPROTOCOL: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_SUBPROTOCOL")
+  lazy val VITE_DISCOVERY_SERVER_WEBSOCKET_PROTOCOL: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_PROTOCOL")
+  lazy val VITE_DISCOVERY_SERVER_WEBSOCKET_HOST: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_HOST")
+  lazy val VITE_DISCOVERY_SERVER_WEBSOCKET_PATH: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_PATH")
+  lazy val VITE_DISCOVERY_SERVER_WEBSOCKET_SUBPROTOCOL: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_SUBPROTOCOL")
   // TODO: whats the difference between the two?
-  val VITE_DISCOVERY_SERVER_WEBSOCKET_LISTEN_PORT: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_LISTEN_PORT")
-  val VITE_DISCOVERY_SERVER_WEBSOCKET_PUBLIC_PORT: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_PUBLIC_PORT")
+  lazy val VITE_DISCOVERY_SERVER_WEBSOCKET_LISTEN_PORT: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_LISTEN_PORT")
+  lazy val VITE_DISCOVERY_SERVER_WEBSOCKET_PUBLIC_PORT: String = Env.get("VITE_DISCOVERY_SERVER_WEBSOCKET_PUBLIC_PORT")
 
-  val VITE_TURN_SERVER_HOST: String = Env.get("VITE_TURN_SERVER_HOST")
-  val VITE_TURN_SERVER_PORT: String = Env.get("VITE_TURN_SERVER_PORT")
-  val VITE_ALWAYS_ONLINE_PEER_PROTOCOL: String = Env.get("VITE_ALWAYS_ONLINE_PEER_PROTOCOL")
-  val VITE_ALWAYS_ONLINE_PEER_HOST: String = Env.get("VITE_ALWAYS_ONLINE_PEER_HOST")
+  lazy val VITE_TURN_SERVER_HOST: String = Env.get("VITE_TURN_SERVER_HOST")
+  lazy val VITE_TURN_SERVER_PORT: String = Env.get("VITE_TURN_SERVER_PORT")
+  lazy val VITE_ALWAYS_ONLINE_PEER_PROTOCOL: String = Env.get("VITE_ALWAYS_ONLINE_PEER_PROTOCOL")
+  lazy val VITE_ALWAYS_ONLINE_PEER_HOST: String = Env.get("VITE_ALWAYS_ONLINE_PEER_HOST")
 
-  val VITE_ALWAYS_ONLINE_PEER_PATH: String = Env.get("VITE_ALWAYS_ONLINE_PEER_PATH")
-  val VITE_ALWAYS_ONLINE_PEER_SUBPROTOCOL: String = Env.get("VITE_ALWAYS_ONLINE_PEER_SUBPROTOCOL")
-  val VITE_ALWAYS_ONLINE_PEER_PUBLIC_PORT: String = Env.get("VITE_ALWAYS_ONLINE_PEER_PUBLIC_PORT")
+  lazy val VITE_ALWAYS_ONLINE_PEER_PATH: String = Env.get("VITE_ALWAYS_ONLINE_PEER_PATH")
+  lazy val VITE_ALWAYS_ONLINE_PEER_SUBPROTOCOL: String = Env.get("VITE_ALWAYS_ONLINE_PEER_SUBPROTOCOL")
+  lazy val VITE_ALWAYS_ONLINE_PEER_PUBLIC_PORT: String = Env.get("VITE_ALWAYS_ONLINE_PEER_PUBLIC_PORT")
 
-  val VITE_DEKANAT_MAIL: String = Env.get("VITE_DEKANAT_MAIL")
+  lazy val VITE_DEKANAT_MAIL: String = Env.get("VITE_DEKANAT_MAIL")
 
   lazy val APP_VERSION: String = js.Dynamic.global.APP_VERSION.asInstanceOf[String]
 
